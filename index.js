22feb788a95eb68ed65d9d4c2cb549c920dd8d9e
Roger M <admin@briefpictures.com>
2021-03-26 13:22:42 -0700

    fixed comment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-# Hardcode now, .env wasn't working
+// Hardcode now, .env wasn't working
 const AWS_ACCESS_KEY_ID = "AKIA3LT2DQXA2B3WATXA"
 const AWS_SECRET_ACCESS_KEY = "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"
 
