2e9e15d97c0e7a6b4a353ee6e160ede38465ef67
Roger M <admin@briefpictures.com>
2021-03-26 10:44:24 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+
+# Hardcode now, .env wasn't working
+const AWS_ACCESS_KEY_ID = "AKIA3LT2DQXA2B3WATXA"
+const AWS_SECRET_ACCESS_KEY = "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"
+
+// TODO
+
